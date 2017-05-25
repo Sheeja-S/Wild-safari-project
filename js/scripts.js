7bd95ec853daa8cfc8896cfebc86da584a69e28d
Sheeja Sankar <sheeja.s18@gmail.com>
2017-05-25 09:53:15 +0800

    project with background video added

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+}
+
+function initMap() {
+  var uluru = {lat: 24.1327, lng:7.0566};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
